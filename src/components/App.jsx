b33720f85b9ee081f195d03354d44a3bc3c62310
Marko Kustudija <markokustudija@Markos-MacBook-Air.local>
2021-05-16 18:49:28 +0200

    practise mapping

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,45 +4,24 @@
 
 function App() {
 
+  function createCards(contact){
+    return <Card 
+                 id = {contact.id}
+                 key = {contact.name}
+                 name = {contact.name}
+                 img = {contact.imgURL}
+                 tel = {contact.phone}
+                 email = {contact.email}
+       />
+  
+  }
+
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      
-      <Card 
-        name = {contacts[0].name}
-        img = {contacts[0].imgURL}
-        tel = {contacts[0].phone}
-        email = {contacts[0].email}
-
-      />
-
-      <Card 
-        name = {contacts[1].name}
-        img = {contacts[1].imgURL}
-        tel = {contacts[1].phone}
-        email = {contacts[1].email}
-
-      />
-
-      <Card 
-        name = {contacts[2].name}
-        img = {contacts[2].imgURL}
-        tel = {contacts[2].phone}
-        email = {contacts[2].email}
-
-      />
-
-
-{/* 
-      <Card 
-        name = "Chuck Noris"
-        img = "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-        tel = "+123 333 789"
-        email = "c@norise.com"
-
-      /> */}
-      
+      {contacts.map(createCards)}
     </div>
+
   );
 
 }
